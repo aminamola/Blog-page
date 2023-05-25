6a479495419da89611adaba23cd2c2e4d3ca791a
aminamola <imaminamoula@gmail.com>
2023-05-25 23:20:07 +0200

    .....

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    // Example: Provide a custom path to a Tailwind config file
-    config: { path: './custom-config.cjs' },
-  })],
-  // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    config: { applyBaseStyles: false },
+  integrations: [tailwind()]
 });
