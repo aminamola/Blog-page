b9513539cd6b1bb43dcb613eb1dac6e3184f3ff7
aminamola <imaminamoula@gmail.com>
2023-05-25 21:39:28 +0200

    ....

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind({
+    // Example: Provide a custom path to a Tailwind config file
+    config: { path: './custom-config.cjs' },
+  })],
+  // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    config: { applyBaseStyles: false },
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'bgBody': "url('/src/assets/Royal.jpg')",
+			}
+		},
+		container: {
+			padding: {
+				DEFAULT: '3rem',
+				sm: '2rem',
+				lg: '4rem',
+				xl: '11rem',
+			    '2xl':'13rem',
+			},
+            },
 	},
-	plugins: [],
+	plugins: [
+		/* */
+		/* */
+	],
 }
